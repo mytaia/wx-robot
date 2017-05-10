5a8d6a9aaca3ea6f0636626730b9a2d4d341c2c8
huchangjiang <huchangjiang@szcomtop.com>
2017-05-10 18:19:45 +0800

    update

---
+++ src/main/java/me/robin/wx/robot/frame/model/req/AbstractBaseRequest.java
@@ -0,0 +1,35 @@
+
+package me.robin.wx.robot.frame.model.req;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * FIXME 类注释信息(此标记自动生成,注释填写完成后请删除)
+ * 
+ * <pre>
+ * [
+ * 调用关系:
+ * 实现接口及父类:
+ * 子类:
+ * 内部类列表:
+ * ]
+ * </pre>
+ * 
+ * @author 作者
+ * @since 1.0
+ * @version 2017年5月4日 作者
+ */
+public class AbstractBaseRequest {
+    
+    /** FIXME */
+    public WxBaseRequest BaseRequest;
+    
+    /**
+     * FIXME 方法注释信息(此标记由Eclipse自动生成,请填写注释信息删除此标记)
+     *
+     * @return x
+     */
+    public String toJsonString() {
+        return JSON.toJSONString(this);
+    }
+}
